728c99c40a6991c0ab2870ed66d0d114d6739a9e
jayesh-zip <patiljayesh1469@gmail.com>
2024-09-28 22:03:13 +0530

    _dirname

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
-const __dirname = path.resolve();
+const _dirname = path.resolve();
 
 const app = express();
 app.use(express.json({ extended: true }));
@@ -22,12 +22,12 @@ app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
 // Serve static files from the React app (after running `npm run build`)
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(_dirname, '/client/build')));
 
 
 // Catch-all route to serve the frontend on any route that is not API
 app.get('*', (_, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
 });
 
 // Error handling middleware
