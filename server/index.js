cede8e9a24e19e4ef5538110bef8df9d937a0976
jayesh-zip <patiljayesh1469@gmail.com>
2024-09-28 21:57:46 +0530

    __dirname

--- server/index.js
+++ server/index.js
@@ -8,26 +8,26 @@ const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
 // Update CORS for production, replace localhost with your deployed frontend domain
-app.use(cors({ credentials: true, origin: "https://blog-app-dp8f.onrender.com" })); // Change this to your production URL
+app.use(cors({ credentials: true, origin: "https://blog-app-dp8f.onrender.com" }));
 
 // API routes
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
 // Serve static files from the React app (after running `npm run build`)
-app.use(express.static(path.join(_dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 
 // Catch-all route to serve the frontend on any route that is not API
 app.get('*', (_, res) => {
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 // Error handling middleware
